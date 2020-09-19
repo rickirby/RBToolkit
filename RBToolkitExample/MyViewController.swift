e12cb7cdfdc24a8a99297ea6516dd52e733df2d9
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-19 20:17:34 +0700

    add example for verifying

--- RBToolkitExample/MyViewController.swift
+++ RBToolkitExample/MyViewController.swift
@@ -11,11 +11,28 @@ import RBToolkit
 
 class MyViewController: UIViewController {
 
+	@IBOutlet weak var imageView: UIImageView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 		title = "Example"
 		setNavigationBarColor(backgroundColor: .orange, textColor: .white, tintColor: .white)
+		imageView.contentMode = .scaleAspectFill
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		DispatchQueue.global(qos: .userInitiated).async {
+			let image = #imageLiteral(resourceName: "earth")
+			let imageSize = image.size
+			let resizedImage = image.createThumbnail(withSize: CGSize(width: imageSize.width / 10, height: imageSize.height / 10))
+			
+			DispatchQueue.main.async {
+				self.imageView.image = resizedImage
+			}
+		}
 	}
 
 	@IBAction func buttonTapped(_ sender: UIButton) {
