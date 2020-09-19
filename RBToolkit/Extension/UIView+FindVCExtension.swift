a8ba69eca63af9ba7286942f6b0a90a876ebfcc8
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-19 20:21:28 +0700

    copy extension

--- RBToolkit/Extension/UIView+FindVCExtension.swift
+++ RBToolkit/Extension/UIView+FindVCExtension.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+	func findViewController() -> UIViewController? {
+		if let nextResponder = self.next as? UIViewController {
+			return nextResponder
+		} else if let nextResponder = self.next as? UIView {
+			return nextResponder.findViewController()
+		} else {
+			return nil
+		}
+	}
+}
