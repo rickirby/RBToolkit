ddb33eb9769b54c4d625f67674333e346a594fa1
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-11-18 21:36:22 +0700

    move extension to designated folder

--- RBToolkit/Base/ViewController.swift
+++ RBToolkit/Base/ViewController.swift
@@ -114,14 +114,3 @@ private extension ViewController {
 		}
 	}
 }
-
-private extension Notification {
-
-	var keyboardAnimationDuration: TimeInterval? {
-		(userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue
-	}
-
-	var keyboardRect: CGRect? {
-		userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
-	}
-}
