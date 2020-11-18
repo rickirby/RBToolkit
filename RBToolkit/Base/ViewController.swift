d74c01af33fbc715fd3ab5c1f59890393dd10f14
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-11-18 21:35:07 +0700

    add some properties on viewcontroller

--- RBToolkit/Base/ViewController.swift
+++ RBToolkit/Base/ViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 open class ViewController<V: View>: UIViewController {
 	
+	public var automaticallyAdjustKeyboardLayoutGuide = false {
+		willSet {
+			newValue ? startObservingKeyboardNotifications() : stopObservingKeyboardNotifications()
+		}
+	}
+	
+	public var onKeyboardWillAppear: ((Notification) -> Void)?
+	public var onKeyboardWillDisappear: ((Notification) -> Void)?
+	
+	private var keyboardNotifications: [Notification.Name] {
+		[
+			UIResponder.keyboardWillHideNotification,
+			UIResponder.keyboardWillShowNotification,
+			UIResponder.keyboardWillChangeFrameNotification
+		]
+	}
+	
 	public var screenView: V {
 		return view as! V
+	}
+	
+	deinit {
+		stopObservingKeyboardNotifications()
 	}
 	
 	open override func loadView() {
@@ -53,4 +74,54 @@ open class ViewController<V: View>: UIViewController {
 		screenView.onViewDidLayoutSubviews()
 	}
 	
+}
+
+private extension ViewController {
+
+	func startObservingKeyboardNotifications() {
+		keyboardNotifications.forEach { registerForNotification(name: $0) }
+	}
+
+	func stopObservingKeyboardNotifications() {
+		keyboardNotifications.forEach { NotificationCenter.default.removeObserver(self, name: $0, object: nil) }
+	}
+
+	func registerForNotification(name: Notification.Name) {
+		NotificationCenter.default.addObserver(forName: name, object: nil, queue: nil, using: handleNotification(name: name))
+	}
+
+	func handleNotification(name: Notification.Name) -> (Notification) -> Void {
+		{ [weak self] notification in
+			guard let self = self, self.automaticallyAdjustKeyboardLayoutGuide, var offset = notification.keyboardRect?.height else {
+				return
+			}
+
+			switch name {
+			case UIResponder.keyboardWillHideNotification:
+				offset = 0
+				self.onKeyboardWillDisappear?(notification)
+			case UIResponder.keyboardWillShowNotification:
+				self.onKeyboardWillAppear?(notification)
+			default:
+				break
+			}
+
+			let animationDuration = notification.keyboardAnimationDuration ?? 0.25
+			self.screenView.keyboardHeightConstraint.constant = offset
+			UIView.animate(withDuration: animationDuration) {
+				self.screenView.layoutIfNeeded()
+			}
+		}
+	}
+}
+
+private extension Notification {
+
+	var keyboardAnimationDuration: TimeInterval? {
+		(userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue
+	}
+
+	var keyboardRect: CGRect? {
+		userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+	}
 }
